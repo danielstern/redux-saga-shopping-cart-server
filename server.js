63d3ef4ddd05b245ae67bd26336bff0c2745f612
Daniel Stern <daniel@danielstern.ca>
2017-06-19 11:44:40 -0400

    added price API

--- server.js
+++ server.js
@@ -26,6 +26,36 @@ nativeObject = YAML.load('database.yml',(result)=>{
         }
     });
 
+    app.get("/prices/:symbol/:ids",(req,res)=>{
+        const ids = req.params.ids.split(',');
+        const items = ids.map(id=>result.items.find(item=>item.id===id));
+        const supportedSymbols = ["CAD","USD"];
+        const symbol = req.params.symbol;
+        if (!supportedSymbols.includes(symbol)) {
+            return res
+                .status(403)
+                .json({
+                    error:"The currency symbol provided is inaccurate, see list of supported currencies",
+                    supportedSymbols
+                })
+        }
+
+
+        if (items.includes(undefined)) {
+            return res
+                .status(500)
+                .json({error:"A specified ID had no matching item"});
+        } else {
+            res
+                .status(200)
+                .json(items.map(item=>({
+                    id: item.id,
+                    symbol,
+                    price:symbol === "USD" ? item.usd : item.cad
+                })));
+        }
+    });
+
     app.listen(port,()=>{
         console.log(`Redux Saga Cart backend server is listening on ${port}`)
     });
