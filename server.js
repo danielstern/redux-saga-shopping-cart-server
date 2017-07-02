ee6e9fbd02088dc3fbbffcc312a2c44632265dd7
Daniel Stern <daniel@danielstern.ca>
2017-07-02 10:57:52 -0400

    adjust delay

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 8081;
 const app = new express();
 YAML = require('yamljs');
 
-const serverDelayConstant = 10;
+const serverDelayConstant = 100;
 // Simulate a small amount of delay to demonstrate app's async features
 app.use((req,res,next)=>{
     const delay = (Math.random() * 15 + 5) * serverDelayConstant;
